31c12805ac07a8cba1b8c34330c2d599209dc30d
selmi youssef <selmiyoussef50@yahoo.fr>
2016-05-11 17:46:43 +0200

    random forest script

--- randomForest.R
+++ randomForest.R
@@ -1,3 +1,4 @@
+### Classification:
 #get the data
 data(iris)
 head(iris)
@@ -18,3 +19,19 @@ pred=predict(model,test)
 Cf=table(pred,test$Species)
 accuracy=sum(diag(model$confusion))/sum(model$confusion)
 error=1-accuracy
+#exemple:nombre de fois ou chaque individu est "out of bag"
+#les 5 premiers
+model$oob.times[1:5]
+#exemple d'echantillon d'individus
+E=c(30,70,60)
+train$Species[E]
+model$oob.times[E]
+model$votes[E,]
+#note:generalement pour chaque arbre le tirage de "bag" est outour
+#de 66%  et 34% out "of bag"
+#importance variable for the split 
+model$importance
+varImpPlot(model)
+plot(model)
+#the positive margin means the correct classification
+plot(margin(model,test$Species))
